4e368c7641a2d2d2a2dcee2230602502ed3405eb
Kenneth Lauer <kenneth.j.lauer@protonmail.com>
2025-08-05 17:11:15 -0400

    Cat Sprites!
    Added the cat sprites and a camera. Programmed the animations. Tweaked the project settings. Reduced the jump height.

--- Scripts/player_controller.gd
+++ Scripts/player_controller.gd
@@ -1,31 +1,61 @@
 extends CharacterBody2D
 
 # Exporting for easy testing. Convert back to consts once the movement is locked
-@export var speed = 300.0
-@export var jump_velocity = -800.0
+@export var speed: float = 300.0
+@export var jump_velocity: float = -600.0
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+@onready var camera_2d = $Camera2D
+var jumping: bool = false
 
 
 func _physics_process(delta):
-	# Add the gravity.
-	if not is_on_floor():
-		velocity += get_gravity() * delta
+	apply_gravity(delta)
 	
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_velocity
+		jumping = true
+		animated_sprite_2d.play("jump")
 	
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("move_left", "move_right")
-	if direction:
-		velocity.x = direction * speed
-	else:
-		velocity.x = move_toward(velocity.x, 0, speed)
+	process_movement_input()
 	
+	# NOT FUNCTIONAL YET
 	attempt_climb(delta)
+	# NOT FUNCTIONAL YET
 	attempt_mantle(delta)
 	
 	move_and_slide()
+
+func apply_gravity(delta) -> void:
+	# Add the gravity.
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+	else:
+		if jumping == true:
+			jumping = false
+			animated_sprite_2d.play("idle")
+
+func process_movement_input() -> void:
+	# Get the input direction and handle the movement/deceleration.
+	var direction = Input.get_axis("move_left", "move_right")
+	if direction:
+		velocity.x = direction * speed
+		# Flip the sprite to face the direction of movement
+		if direction > 0:
+			animated_sprite_2d.flip_h = false
+		else:
+			animated_sprite_2d.flip_h = true
+		# Only play the move animation when not jumping
+		# BUG: the walking animation will continue when walking off a ledge because that is not a "jump"
+		# Solution: the flag should be changed from "jumping" to "airborne" and be set to true whenever
+		# our cat is not on the floor
+		if not jumping:
+			animated_sprite_2d.play("move")
+	else:
+		velocity.x = move_toward(velocity.x, 0, speed)
+		# Only play this animation when on the floor
+		if not jumping:
+			animated_sprite_2d.play("idle")
 
 func attempt_mantle(delta) -> void:
 	if is_on_wall():
@@ -44,6 +74,6 @@ func attempt_climb(delta) -> void:
 		# If the cat is touching a climbable wall it sticks to that wall and slowly climbs
 		pass
 
-
+# Testing for the wall detection. Doesn't seem helpful yet...
 func _on_area_2d_body_entered(body):
 	print(body.get_collision_layer())
